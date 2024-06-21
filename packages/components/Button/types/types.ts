0c4c21f0cdf3961bd923d9288b234e3a4ebff0ab
YovyDebugger9689 <2589524839@qq.com>
2024-06-21 19:18:09 +0800

    :bug: 为Button组件实例新增了disabled, size, type属性

--- packages/components/Button/types/types.ts
+++ packages/components/Button/types/types.ts
@@ -1,9 +1,10 @@
-import type { Component, Ref } from "vue";
+import type { Component, ComputedRef, Ref } from "vue";
 
 export type ButtonType = "primary" |	"success" |	"warning" |	"danger" | "info"
 export type NativeType = "button" |	"reset" |	"submit"
 export type ButtonSize = "large" | "default" | "small"
 
+// YoButton实例对象的props属性
 export interface ButtonProps {
   tag?: string | Component;
   type?: ButtonType;
@@ -21,20 +22,27 @@ export interface ButtonProps {
   throttleDuration?: number;
 }
 
+// YoButton触发的事件
 export interface ButtonEmits {
   (e: 'click', val: MouseEvent): void
 }
 
+// YoButton的组件实例
 export interface ButtonInstance {
   ref: Ref<HTMLButtonElement | void>
+  disabled: ComputedRef<boolean>
+  size: ComputedRef<ButtonSize | "">
+  type: ComputedRef<ButtonType | "">
 }
 
+// YoButtonGroup的props属性
 export interface ButtonGroupProps {
   size?: ButtonSize
   type?: ButtonType
   disabled?: boolean
 }
 
+// YoButtonGroup的组件实例
 export interface ButtonGroupContext {
   size?: ButtonSize
   type?: ButtonType
