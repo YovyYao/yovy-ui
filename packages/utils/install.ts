6c9b8486d07cc6e13ba368b811e59b9615049012
YovyDebugger9689 <2589524839@qq.com>
2024-06-22 23:46:20 +0800

    :wrench: 修改代码注释

--- packages/utils/install.ts
+++ packages/utils/install.ts
@@ -3,13 +3,22 @@ import { each } from 'lodash-es';
 
 type SFCWithinstall<T> = T & Plugin
 
-// 全局引入
+/**
+ * 遍历组件数组, 为每一个组件使用use函数, 以达到全局引入的目的
+ * @param components 组件数组
+ * @returns 返回一个遍历了所有组件之后的install函数, 作为参数提供给App.use()
+ */
 export function makeInstaller(components: Plugin[]) {
 	const installer = (app: App) => each(components, c => app.use(c))
 	
 	return installer as Plugin
 }
 
+/**
+ * 为组件添加install函数, install函数里为组件命名, 以及调用component函数
+ * @param component Vue组件
+ * @returns 拥有install函数的组件对象
+ */
 export const withInstall = <T>(component: T) => {
 	(component as SFCWithinstall<T>).install = (app: App) => {
 		const name = (component as any).name
