01d0798d5a7500562929ae6d7c74d7a4e89e0965
YovyY <yovywio@gmail.com>
2024-07-04 15:23:54 +0800

    :pencil: 完成了注释的编写并在Tooltip的utils中导出

--- packages/components/Tooltip/utils/eventOnTriggerNode.ts
+++ packages/components/Tooltip/utils/eventOnTriggerNode.ts
@@ -5,10 +5,10 @@ import type { ComputedRef, Ref, WatchStopHandle } from 'vue';
 import type { TooltipProps } from '../types';
 
 /**
- * 
+ * 绑定虚拟触发节点和事件监听. Tooltip组件组要知道, 它的虚拟节点和事件什么时候会被触发. (事件: hover, click, contextmenu)
  * @param props Tooltip组件的属性和一个可选的虚拟触发器属性
  * @param triggerNode 基于vue的ComputedRef, 将该参数变成computed计算属性, 该属性可能是HTMLElement类型或void类型
- * @param events 基于vue的Ref, 将参数变成Ref对象, 
+ * @param events 基于vue的Ref, 将参数变成Ref对象.(hover, click, contextmenu)
  * @param closeMethod 
  */
 export function eventsOnTriggerNode(
@@ -23,7 +23,7 @@ export function eventsOnTriggerNode(
 	const _eventHandleMap = new Map()
 
 	/**
-	 * 绑定虚拟节点上的触发器
+	 * 绑定虚拟节点上的触发器. 监听events(一个事件, hover, click, contextmenu)
 	 */
 	const _bindEventOnVirtualTrigger = () => {
 		const el = triggerNode.value
@@ -35,7 +35,7 @@ export function eventsOnTriggerNode(
 	}
 	
 	/**
-	 * 解绑虚拟节点上的触发器
+	 * 解绑虚拟节点上的触发器. 
 	 */
 	const _unbindEventOnVirtualTrigger = () => {
 		const el = triggerNode.value
@@ -46,6 +46,10 @@ export function eventsOnTriggerNode(
 			)
 	}
 
+	/**
+	 * 监听triggerNode. 当开启了虚拟节点的触发时, 调用_bindEventOnVirtualTrigger()绑定虚拟节点触发.
+	 * ps: Vue3中的 watch 有返回值, 其返回值的类型是WatchStopHandle, 返回的值可用于停止监听.
+	 */
 	onMounted(() => {
 		watchTriggerNodeStopHandle = watch(
 			triggerNode,
@@ -54,6 +58,9 @@ export function eventsOnTriggerNode(
 		)
 	})
 
+	/**
+	 * 监听events. 如果开启了虚拟节点的触发, 则选解绑, 再绑定. 在调用closeMethod()
+	 */
 	watchEventsStopHandle = watch(
 		events,
 		() => {
@@ -65,6 +72,9 @@ export function eventsOnTriggerNode(
 		{ deep: true },
 	)
 
+	/**
+	 * 虚拟节点卸载后, 停止对其事件和触发的监听.
+	 */
 	onUnmounted(() => {
 		watchTriggerNodeStopHandle?.()
 		watchEventsStopHandle?.()
--- packages/components/Tooltip/utils/index.ts
+++ packages/components/Tooltip/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './clickOutsideElement'
 export * from './eventListener'
+export * from './eventOnTriggerNode'
